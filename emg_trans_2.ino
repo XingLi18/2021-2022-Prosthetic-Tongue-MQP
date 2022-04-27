90e97d596be13fc2c046b61d69345efe8180e3cd
Xavier Curney <xaviercurney2018@gmail.com>
2022-04-27 00:11:33 -0400

    Create emg_trans_2.ino

---
+++ emg_trans_2.ino
@@ -0,0 +1,65 @@
+#include <SPI.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE (yellow), CSN (orange)         
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+//emg will signal output
+
+int firstFiveTime = 0;                //determines if the code has run 5 times to average for threshold
+
+int muscle = 0;
+
+//some default threshold value
+float emgThreshold = 2.5;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();          //This sets the module as transmitter
+}
+
+void loop() {
+  // read the input on analog pin 0:
+  int emgValue = analogRead(A0);
+  
+ if (emgValue > 0) {//checks to make sure emg sensor is on the person I suppose
+  if(firstFiveTime < 5){
+    firstFiveTime++;
+    emgThreshold = (emgValue + emgThreshold)/2;
+    emgThreshold = emgThreshold + 50;
+    Serial.println("Threshold update"); 
+  }
+  Serial.print("EMG Threshold ="); 
+  Serial.println(emgThreshold);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  if(emgValue > emgThreshold){//This is where the servo gets turned on and off
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    muscle = 1;
+    //check emg value
+    Serial.print("EMG value = ");
+    Serial.println(emgValue);
+    Serial.println("Muscle ///////////////////////////////////////////////////////////////////////////////////////////////////////"); 
+    
+    }
+ else {
+  muscle = 0;
+    Serial.print("EMG value = ");
+    Serial.println(emgValue);
+    Serial.println("No muscle"); 
+    
+ }
+
+ //check that proper stuff is sending
+  Serial.print("How many muscle you have? ");
+  Serial.println(muscle);
+  radio.write(&muscle, sizeof(muscle));  
+  //radio.write(&emgValue, sizeof(emgValue));  //Sending the message to receiver 
+  //radio.write(&emgThreshold, sizeof(emgThreshold));  //Sending the message to receiver 
+  
+  delay(100);//will code this out later
+}
+}
+
